96732d58c776e0a1de4da9b0eb1a672e6ece10be
samuelArenas2005 <e.sir.samuel.arenas@cali.edu.co>
2026-07-17 20:43:05 -0500

    feat: add new project instance data files and generation script

---
+++ src/gen_instances.py
@@ -0,0 +1,46 @@
+import os
+import random
+import math
+
+def generate_mpl(n, m, filename):
+
+    p = [0]*m
+    for _ in range(n):
+        p[random.randint(0, m-1)] += 1
+    
+ 
+    v = sorted([round(random.uniform(0, 1), 3) for _ in range(m)])
+
+    ce = [round(random.uniform(1.0, 20.0), 3) for _ in range(m)]
+    
+    # c matrix
+    c = []
+    for i in range(m):
+        row = []
+        for j in range(m):
+            if i == j:
+                row.append(0.0)
+            else:
+                row.append(round(random.uniform(0.1, 5.0), 3))
+        c.append(row)
+        
+    ct = round(n * 20.0, 2)
+    max_movs = int(n * 2)
+    
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    with open(filename, "w") as f:
+        f.write(f"{n}\n")
+        f.write(f"{m}\n")
+        f.write(",".join(map(str, p)) + "\n")
+        f.write(",".join(map(str, v)) + "\n")
+        f.write(",".join(map(str, ce)) + "\n")
+        
+        for row in c:
+            f.write(",".join(map(str, row)) + "\n")
+            
+        f.write(f"{ct}\n")
+        f.write(f"{max_movs}\n")
+    print(f"Generated {filename} (n={n}, m={m})")
+
+
+generate_mpl(500000000, 200, r"c:\Users\samue\OneDrive\Documentos\ada_ii_pl_polarizacion\src\MisInstancias\stress_3_n1000000_m200.mpl")
